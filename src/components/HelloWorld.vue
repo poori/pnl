4f8b3925f4a971aa8f81a9ae36212fedd03d1f8e
n <nori.yoshida@gmail.com>
2021-01-25 23:29:53 -0800

    get something up

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,196 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="row">
+    <h1>Polymarket Analyzer and Tax Form Generator</h1>
+    <p>Tool to show better profit and loss stats for your polymarket bets</p>
+
+    <p>Coming soon: Polymarket tax filings. Easy way to download a sheet for filing Polymarket winnings on your taxes</p>
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSexM3QKVdcrdIfVG3Y7Oka5K_JtRwfZ2XUNR0t-PZdnqidlwA/viewform?embedded=true" width="640" height="475" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <!-- name -->
+<p></p>
+      <div class="col-10">
+        <input v-model="address" type="text" class="form-control form-control-lg" placeholder="Enter Your Polymarket Wallet Address (not working yet)" name="address">
+      </div>
+
+      <!-- submit button -->
+      <div class="col-auto">
+        <button v-on:click="findTransactions" type="submit" class="btn btn-lg btn-primary">Find</button>
+      </div>
+      <h3>Redemption History </h3>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+
+            <th scope="col">Description</th>
+            <th scope="col">Num Tokens</th>
+            <th scope="col">Value</th>
+            <th scope="col">Date</th>
+            <th scope="col">Tx hash</th>            
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="redemption in resa.data.account.redemptions" :key="redemption.id">
+
+            <td>{{ redemption.condition.id | getMarketNameByConditionId }}</td>
+            <td>{{ redemption.payout | formatTokenQty }}</td>
+            <td>{{ redemption.payout | formatUSDC }}</td>
+            <td>{{ redemption.condition.resolutionTimestamp | formatDate }}</td>
+            <td>{{redemption.id}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>Transaction History </h3>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Description</th>
+            <th scope="col">Type</th>
+            <th scope="col">Num Tokens</th>
+            <th scope="col">Value (USDC)</th>
+            <th scope="col">Date</th>
+            <th scope="col">Tx Hash</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="transaction in resa.data.account.transactions" :key="transaction.id">
+
+            <td>{{ transaction.market.id | getMarketName}}</td>
+            <td>{{ transaction.type }}</td> 
+            <td>{{ transaction.outcomeTokensAmount | formatTokenQty }}</td>
+            <td>{{ transaction.tradeAmount | formatUSDC }}</td>
+            <td>{{ transaction.timestamp | formatDate }}</td>
+            <td>{{transaction.id}}</td>
+          </tr>
+        </tbody>
+      </table>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+import moment from 'moment'
+import stub_account1 from '../../testdata/account1.json'
+import markets from '../../testdata/allmarkets.json'
+
+
+//preprocess markets into a hash datastructure for faster name lookup
+var markets_hash = {}
+markets.forEach(function (el, i, arr) {
+    let normalized_id = el.marketMakerAddress.toLowerCase()
+    markets_hash[normalized_id] = el; //need to do lowercase b/c for some reason upper and lower are mixed in this file
+})
+
+//preprocess markets into a hash datastructure for faster name lookup
+var markets_conditionid_hash = {}
+markets.forEach(function (el, i, arr) {
+    let normalized_id = el.conditionId.toLowerCase()
+    markets_conditionid_hash[normalized_id] = el; //need to do lowercase b/c for some reason upper and lower are mixed in this file
+})
+//query
+const q_test = gql`
+query {
+  account(id: "0x896e5e594ddf322cd180f01df263e0f22ac07a83") {
+    id
+  }
+}
+`
+const q2 = gql`
+query {
+  account(id: "0x896e5e594ddf322cd180f01df263e0f22ac07a83") {
+    id
+    transactions {
+      id
+      type
+      timestamp
+      market { id }
+      tradeAmount
+      outcomeIndex
+      outcomeTokensAmount
+      feeAmount
+    }
+    redemptions {
+      id     
+      collateralToken {
+        id
+      }
+      condition {
+        resolutionTimestamp
+        id
+      }
+      indexSets
+      payout     
+    }
+  }
+}
+`
+
+async function getTransactionsAndRedemptions(context) {
+  this.address
+  console.log(stub_account1)
+  context.resa = stub_account1
+  //context.$apollo.provider.defaultClient.query({query: q2}).then(res => {context.resa = res; console.log(res)})
+
+}
+
+
+
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() { 
+    return {
+      resa: stub_account1,
+      address: ''
+    }
+  },
+  methods: {
+    async findTransactions() {
+      //let res = await this.$apollo.provider.defaultClient.query({query})
+      //this.resa = res;
+      getTransactionsAndRedemptions(this)
+    }
+  },
+  filters: {
+    formatDate: function (value) {
+      if (value) {
+        return moment.unix(String(value)).format('MM/DD/YYYY hh:mm')
+      }
+    },
+    formatUSDC: function (value) {
+      if (value) {
+        let normalizedUSDC =  value / 1000000
+        return '$' + normalizedUSDC.toFixed(5)
+      }      
+    },
+    formatTokenQty: function (value) {
+      if (value) {
+        return  value / 1000000
+      }     
+    },
+    getMarketName: function (hash) {
+      let name = markets_hash[hash]
+      if (name) {
+        return name.question
+      }
+      return hash
+    },
+    getMarketNameByConditionId: function (hash) {
+      let name = markets_conditionid_hash[hash]
+      if (name) {
+        return name.question
+      }
+      return hash
+    },
+    
   }
+  // apollo: {
+  //   account: {
+  //     query: query
+  //   }
+  // }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,44 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// GRAPHQL STUFF
+import VueCompositionApi, { provide } from '@vue/composition-api'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { createHttpLink } from 'apollo-link-http'
+import {ApolloClient, gql} from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
+const POLYMARKET_URL = "https://subgraph-matic.poly.market/subgraphs/name/TokenUnion/polymarket"
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: POLYMARKET_URL,
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// client apollo client
+const defaultClient = new ApolloClient({
+  link: httpLink,
+  cache: cache,
+  fetchOptions: { mode: 'no-cors' },
+})
+
+
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: defaultClient
+});
+// defaultClient.query({query}).then(res => console.log(res))
+
+
 new Vue({
+  el: '#app',
+  // inject apolloProvider here like vue-router or vuex
+  apolloProvider,
   render: h => h(App),
-}).$mount('#app')
+})
